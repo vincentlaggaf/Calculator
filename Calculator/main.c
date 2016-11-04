07bfd41a5f1bc949b22279390c285467bc5df5b2
Xavier gistau <xavier.gistau@viacesi.fr>
2016-11-04 15:32:00 +0100

    Ajout d'une addition de deux int

--- Calculator/main.c
+++ Calculator/main.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int addition(int a,int b)
+{
+    return a+b;
+}
+
+
+
 int main()
 {
-    printf("Hello world!\n");
+    printf("%d\n",addition(1,4));
     return 0;
 }
